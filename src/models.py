a7186873b06aeeef0b92f56cb1c00a2e11246bec
diegomatah <diegoa.matah@gmail.com>
2022-08-19 01:20:15 +0000

    tarea 2

--- src/models.py
+++ src/models.py
@@ -9,7 +9,7 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
@@ -18,10 +18,10 @@ class User(db.Model):
             # do not serialize the password, its a security breach
         }
 
-    class People(db.Model):
-     uid = db.Column(db.integer,primary_key=True)
-     name = db.Column (db.String(80), unique = False, nullable = False)
-     homeworld = db.Column (db.String(250), unique = True, nullable = True)
+class People(db.Model):
+    uid = db.Column(db.Integer,primary_key=True)
+    name = db.Column (db.String(80), unique = False, nullable = False)
+    homeworld = db.Column (db.String(250), unique = True, nullable = True)
 
     def __repr__(self):
            return '<People %r>' %self.name
@@ -35,7 +35,47 @@ class User(db.Model):
 
 class FavPeople(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user = db.Column(db.String(120), db.ForeingKey("user.email"))
-    people = db.Column(db.Integer, db.ForeingKey("people.uid"))
+    user = db.Column(db.String(120), db.ForeignKey("user.email"))
+    people = db.Column(db.Integer, db.ForeignKey("people.uid"))
     rel_user = db.relationship('User') 
     rel_people = db.relationship('People')
+
+    def __repr__(self):
+           return '<FavPeople %r>' %self.id
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "user": self.user,
+            "people": self.people,
+        }
+
+class Planetas(db.Model):
+    uid = db.Column(db.Integer,primary_key=True)
+    name = db.Column (db.String(80), unique = True, nullable = False)
+
+    def __repr__(self):
+            return '<Planetas %r>' %self.name
+
+    def serialize(self):
+            return{
+                "uid": self.uid,
+                "name": self.name,
+            }
+
+class FavPlanetas(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.String(120), db.ForeignKey("user.email"))
+    planetas = db.Column(db.String(80), db.ForeignKey("planetas.name"))
+    rel_user = db.relationship('User') 
+    rel_planetas = db.relationship('Planetas')
+
+    def __repr__(self):
+           return '<FavPlanetas %r>' %self.id
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "user": self.user,
+            "planetas": self.planetas,
+        }
